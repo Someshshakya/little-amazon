368b6b03edcf24e16853a8bafb4c6bba69527352
someshshakya <someshshakya>
2023-05-31 18:34:49 +0530

    passed data as props and removed static data, refactor the code according to module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React,{Component} from 'react';
 import classes from './App.module.css'
-import ProductPreview from './ProductPreview';
-import ProductDetails from './ProductDetails';
-import TopBar from './TopBar';
+import ProductPreview from './ProductPreview/ProductPreview';
+import ProductDetails from './ProductDetails/ProductDetails';
+import TopBar from './TopBar/TopBar';
+import ProductData from './Utils/ProductData';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    productData: ProductData,
+    currentPreviewImage: 'https://imgur.com/xSIK4M8.png',
+    showHeartBeatSection:false
+  }
+  render() {
+    return (
+      <div className="App">
         {/* top nav bar */}
-         <TopBar/>
-      <div className={classes.MainContainer}>
-        {/* product preview  section*/}
-        <div className={classes.ProductPreview}>
-          <ProductPreview />
-        </div>
-        <div className={classes.ProductData}>
-          {/* product details section  */}
-          <ProductDetails />
+        <TopBar />
+        <div className={classes.MainContainer}>
+          {/* product preview  section*/}
+          <div className={classes.ProductPreview}>
+            <ProductPreview
+              currentPreviewImage={this.state.currentPreviewImage}
+              showHeartBeatSection={this.state.showHeartBeatSection}
+            />
           </div>
-       </div>
-    </div>
-  );
+          <div className={classes.ProductData}>
+            {/* product details section  */}
+            <ProductDetails data={this.state.productData} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ProductDetails/ProductDetails.js
@@ -0,0 +1,40 @@
+import React from "react";
+import classes from './ProductDetails.module.css'
+
+const ProductDetails = function (props) {
+    const colorOptions = props.data.colorOptions.map((item, pos) => {
+        const classArr = [classes.ProductImage]
+        if (pos === 0) {
+            classArr.push(classes.SelectedProductImage)
+        }
+        return (
+            <img className={classArr.join(' ')} key={pos} src={item.imageUrl} alt={item.styleName}></img>
+         )
+    })
+    const featureList = props.data.featureList.map((item, pos) => {
+        const classArr = [classes.FeaturesItem]
+        if (pos === 0) {
+            classArr.push(classes.SelectedFeatureItem)
+        }
+        return (
+            <button key={pos} className={classArr.join(' ')}>{item}</button>
+
+        )
+    })
+    return (
+        <div className={classes.ProductData}>
+              <h2 className={classes.ProductTitle}>{props.data.title}</h2>
+              <p className={classes.ProductDescription}>{props.data.description}</p>
+            <h3 className={classes.Sectionheading}>Select Color</h3>
+            {colorOptions}
+              <h3 className={classes.Sectionheading}>Features</h3>
+              <div>
+                {/* <button className={[classes.FeaturesItem,classes.SelectedFeatureItem].join(' ')}>Time</button>
+                <button className={classes.FeaturesItem}>Heart Rate</button>  */}
+                {featureList}
+              </div>
+              <button className={classes.PrimaryButton}>Buy Now</button>
+           </div>
+    )
+}
+export default ProductDetails;
